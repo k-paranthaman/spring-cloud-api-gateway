ffaa42144283a08d0b11dafca13fa51daf0793db
paranthamank <paranthaman.kuppurathinam@rabobank.nl>
2025-06-12 18:27:14 +0530

    user id change

--- jws-service/src/main/java/com/example/jws/producer/GenerateToken.java
+++ jws-service/src/main/java/com/example/jws/producer/GenerateToken.java
@@ -1,12 +1,12 @@
 package com.example.jws.producer;
 
-import com.nimbusds.jose.*;
-import com.nimbusds.jose.crypto.*;
-import com.nimbusds.jwt.*;
+import com.nimbusds.jose.JWSAlgorithm;
+import com.nimbusds.jose.JWSHeader;
+import com.nimbusds.jose.JWSSigner;
+import com.nimbusds.jose.crypto.RSASSASigner;
+import com.nimbusds.jwt.JWTClaimsSet;
+import com.nimbusds.jwt.SignedJWT;
 
-import java.security.KeyPair;
-import java.security.KeyPairGenerator;
-import java.security.NoSuchAlgorithmException;
 import java.security.interfaces.RSAPrivateKey;
 import java.util.Date;
 import java.util.UUID;
@@ -22,7 +22,7 @@ public class GenerateToken {
         JWTClaimsSet claimsSet = new JWTClaimsSet.Builder()
                 .subject("user123")
                 .issuer("your-app")
-                .claim("uamid", UUID.randomUUID().toString())
+                .claim("userId", UUID.randomUUID().toString())
                 .claim("deviceId", "device-456")
                 .expirationTime(new Date(new Date().getTime() + 60 * 60 * 1000)) // 1 hour expiry
                 .build();
@@ -40,24 +40,4 @@ public class GenerateToken {
         return signedJWT.serialize();
     }
 
-    public void getKey() {
-        KeyPairGenerator keyGen = null;
-        try {
-            keyGen = KeyPairGenerator.getInstance("RSA");
-        } catch (NoSuchAlgorithmException e) {
-            throw new RuntimeException(e);
-        }
-        keyGen.initialize(2048);
-        KeyPair keyPair = keyGen.generateKeyPair();
-        RSAPrivateKey privateKey = (RSAPrivateKey) keyPair.getPrivate();
-
-        // Call the generateToken method
-        String jwt = null;
-        try {
-            jwt = GenerateToken.generateToken(privateKey, "my-key-id");
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-        System.out.println(jwt);
-    }
 }
